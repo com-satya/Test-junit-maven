9da753752f39b175b4beab429efb3c8e5156e429
satyainv <satyainv459@gmail.com>
2022-07-06 01:48:34 +0200

    satya first commit

---
+++ src/test/java/com/inv/maven/jenkins/MavenJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inv.maven.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
